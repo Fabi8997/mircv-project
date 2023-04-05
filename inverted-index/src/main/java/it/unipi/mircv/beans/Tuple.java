6ed16a1a7b96f1da35c173dcbb50adce699a53bd
Fabiano Pilia <83593602+Fabi8997@users.noreply.github.com>
2023-04-05 18:31:29 +0200

    Implemented the skeleton of the scoring based on daat:
    scoreCollection() to score the documents in daat way
    minDoc() to retrieve the minDocId and the list of posting lists with it
    allPostingsEnded() no more postings in each posting list
    
    Added a bean class Tuple to handle a generic tuple (a, b)

---
+++ inverted-index/src/main/java/it/unipi/mircv/beans/Tuple.java
@@ -0,0 +1,20 @@
+package it.unipi.mircv.beans;
+
+public class Tuple<K, V> {
+
+    private final K first;
+    private final V second;
+
+    public Tuple(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+}
