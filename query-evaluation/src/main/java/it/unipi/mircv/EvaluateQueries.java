31600eac69271e2019d68e5d30788bb8f5379751
Fabiano Pilia <83593602+Fabi8997@users.noreply.github.com>
2023-05-27 18:10:26 +0200

    Implemented the writing of the results of the queries into a file, removed the duplicates in the query, test of the module and test the results with trec_eval.

--- query-evaluation/src/main/java/it/unipi/mircv/EvaluateQueries.java
+++ query-evaluation/src/main/java/it/unipi/mircv/EvaluateQueries.java
@@ -11,14 +11,56 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Hello world!
- *
+ * Main class for the query evaluation module, provide the methods to load a batch of queries used to generate a file
+ * containing the results of the queries in a format that can be used to compute the metrics using the trec_eval tool.
  */
 public class EvaluateQueries{
 
+    //Path to the input file containing the queries
     static final String QUERY_PATH = "Dataset/queries.tsv.gz";
 
+    //Path to the output file containing the results of the queries
+    static final String RESULTS_PATH = "Files/queries_results.txt";
+
+    //bm25scoring flag indicating if the scoring is bm25 (true) or tfidf (false)
+    static final Boolean bm25scoring = true;
+
+    //flag indicating if the query is disjunctive (true) or disjunctive (false)
+    static final Boolean queryType = true;
+
+    
+    public static void main( String[] args )
+    {
+
+        Configuration configuration = new Configuration();
+
+        //If no configuration is found, then no inverted index is present. The program exits.
+        if(!configuration.loadConfiguration())
+            return;
+
+        System.out.println("[QUERY PROCESSOR] Building inverted index configuration:");
+        System.out.println(configuration);
+
+        System.out.println("[QUERY PROCESSOR] Loading the lexicon in memory...");
+        Lexicon lexicon = new Lexicon();
+        lexicon.loadLexicon();
+
+        System.out.println("[QUERY PROCESSOR] Loading the document index in memory...");
+        DocumentIndex documentIndex = new DocumentIndex();
+        documentIndex.loadDocumentIndex();
+
+        evaluateQueries(getQueries(), configuration, documentIndex, lexicon);
+
+
+    }
+
+    /**
+     * Read from a file a list of queries in the format of qid\tquery and return an array of tuple containing the
+     * qid and the query: (qid, query)
+     * @return an array of tuple containing the qid and the query: (qid, query)
+     */
     private static ArrayList<Tuple<Long, String>> getQueries(){
+
         //Path of the collection to be read
         File file = new File(QUERY_PATH);
 
@@ -40,10 +82,12 @@ public class EvaluateQueries{
                 //Iterate over the lines
                 while ((line = bufferedReader.readLine()) != null ) {
 
+                    //Split the line qid\tquery in qid query
                     String[] split = line.split("\t");
 
+                    //Add it to the results array
                     if(split[0] != null && split[1] != null) {
-                        results.add(new Tuple<>(Long.getLong(split[0]), split[1]));
+                        results.add(new Tuple<>(Long.parseLong(split[0]), split[1]));
                     }
                 }
 
@@ -54,93 +98,108 @@ public class EvaluateQueries{
         }
     }
 
-    private static void evaluateQueries(ArrayList<Tuple<Long,String>> queries, Configuration configuration, DocumentIndex documentIndex, Lexicon lexicon, Boolean bm25scoring, Boolean queryType){
+    /**
+     * Performs all the queries in the array of queries, using the configuration parameters passed, for the scoring it
+     * requires the document index and the lexicon.
+     * @param queries array of tuples containing the query id and the query string (queryId, query)
+     * @param configuration configuration used during the creation of the previous inverted index
+     * @param documentIndex document index containing the document info
+     * @param lexicon lexicon containing the terms information
+     */
+    private static void evaluateQueries(ArrayList<Tuple<Long,String>> queries, Configuration configuration, DocumentIndex documentIndex, Lexicon lexicon){
 
-        for( Tuple<Long,String> tuple : queries ){
+        //Object used to build the lexicon line into a string
+        StringBuilder stringBuilder;
 
-            //Read the next query
-            String query = "-1\t" + tuple.getSecond();
+        //Buffered writer used to format the output
+        BufferedWriter bufferedWriter;
 
+        try {
 
-            //Parse the query
-            String[] queryTerms = parseQuery(query, lexicon, configuration.getStemmingAndStopwordsRemoval());
+            bufferedWriter = new BufferedWriter(new FileWriter(RESULTS_PATH,false));
 
-            System.out.println("Query: " + query + "\t" + "Terms: " + Arrays.toString(queryTerms));
+            // TODO: 27/05/2023 compute the avg of the completion time
 
-            //If the query string is equal to null it means that the query contains all stopwords or all the terms
-            // were written in a bad way or not present in the lexicon.
-            if(queryTerms == null || queryTerms.length == 0) {
-                System.out.println("You're query is too vague, try to reformulate it.");
-                continue;
+            for( Tuple<Long,String> tuple : queries ){
+
+                //Read the next query, add -1 to indicate that it is a query
+                String query = "-1\t" + tuple.getSecond();
+
+                // TODO: 27/05/2023 For each query compute the ms to execute it
+
+                //Parse the query
+                String[] queryTerms = parseQuery(query, lexicon, configuration.getStemmingAndStopwordsRemoval());
+
+                System.out.println("Query: " + query + "\t" + "Terms: " + Arrays.toString(queryTerms));
+
+                //If the query string is equal to null it means that the query contains all stopwords or all the terms
+                // were written in a bad way or not present in the lexicon.
+                if(queryTerms == null || queryTerms.length == 0) {
+                    System.out.println("You're query is too vague, try to reformulate it.");
+                    continue;
+                }
+
+                //Remove the duplicates
+                queryTerms = Arrays.stream(queryTerms).distinct().toArray(String[]::new);
+
+                System.out.println("Query: " + query + "\t" + "Terms: " + Arrays.toString(queryTerms));
+
+                //Load the posting list of the terms of the query
+                PostingList[] postingLists = new PostingList[queryTerms.length];
+
+                //For each term in the query terms array
+                for (int i = 0; i < queryTerms.length; i++) {
+
+                    //Instantiate the posting for the i-th query term
+                    postingLists[i] = new PostingList();
+
+                    //Load in memory the posting list of the i-th query term
+                    postingLists[i].openList(lexicon.get(queryTerms[i]));
+
+                }
+
+                //Array to hold the results of the query
+                ArrayList<Tuple<Long, Double>> result;
+
+                //Score the collection
+                if(queryType){
+                    result = Score.scoreCollectionDisjunctive(postingLists,documentIndex, bm25scoring, false);
+                }else {
+                    result = Score.scoreCollectionConjunctive(postingLists,documentIndex, bm25scoring, false);
+                }
+
+                //Write the results in a format valid for the TREC_EVAL tool
+                for(int i = 0; i < result.size(); i++){
+
+                    //New string builder for the current result
+                    stringBuilder = new StringBuilder();
+
+                    //build the string
+                    stringBuilder
+                            .append(tuple.getFirst()).append(" ")
+                            .append("q0 ")
+                            .append(documentIndex.get(result.get(i).getFirst()).getDocNo()).append(" ")
+                            .append(i+1).append(" ")
+                            .append(result.get(i).getSecond()).append(" ")
+                            .append("runid1").append("\n");
+
+                    //Write the string in the file
+                    bufferedWriter.write(stringBuilder.toString());
+
+                }
+
+                //Close the posting lists
+                for (PostingList postingList : postingLists) {
+                    postingList.closeList();
+                }
+
             }
 
-            //Load the posting list of the terms of the query
-            PostingList[] postingLists = new PostingList[queryTerms.length];
-
-            //For each term in the query terms array
-            for (int i = 0; i < queryTerms.length; i++) {
-
-                //Instantiate the posting for the i-th query term
-                postingLists[i] = new PostingList();
-
-                //Load in memory the posting list of the i-th query term
-                postingLists[i].openList(lexicon.get(queryTerms[i]));
-
-            }
-
-            ArrayList<Tuple<Long, Double>> result;
-
-            //Score the collection
-            if(queryType){
-                result = Score.scoreCollectionDisjunctive(postingLists,documentIndex, bm25scoring, false);
-            }else {
-                result = Score.scoreCollectionConjunctive(postingLists,documentIndex, bm25scoring, false);
-            }
-
-            // TODO: 26/05/2023 Instead of printing the results, write it to the file 
-            //Print the results in a formatted way
-            System.out.println("\n#\tDOCNO\t\tSCORE");
-            for(int i = 0; i < result.size(); i++){
-                System.out.println((i+1) +
-                        ")\t" +
-                        documentIndex.get(result.get(i).getFirst()).getDocNo() +
-                        "\t"+result.get(i).getSecond());
-            }
-
-            System.out.println();
-
-            //Close the posting lists
-            for (PostingList postingList : postingLists) {
-                postingList.closeList();
-            }
-
-
+            //Close the writer
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-    }
-
-    public static void main( String[] args )
-    {
-
-        Configuration configuration = new Configuration();
-
-        //If no configuration is found, then no inverted index is present. The program exits.
-        if(!configuration.loadConfiguration())
-            return;
-
-        System.out.println("[QUERY PROCESSOR] Building inverted index configuration:");
-        System.out.println(configuration);
-
-        System.out.println("[QUERY PROCESSOR] Loading the lexicon in memory...");
-        Lexicon lexicon = new Lexicon();
-        lexicon.loadLexicon();
-
-        System.out.println("[QUERY PROCESSOR] Loading the document index in memory...");
-        DocumentIndex documentIndex = new DocumentIndex();
-        documentIndex.loadDocumentIndex();
-
-        evaluateQueries(getQueries(), configuration, documentIndex, lexicon, true, true);
-
     }
 
     /**
@@ -175,4 +234,6 @@ public class EvaluateQueries{
         //Return an array of String containing the results of the parsing process
         return results.toArray(new String[0]);
     }
+
+
 }
